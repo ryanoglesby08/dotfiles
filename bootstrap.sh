6ba4fcb38068265b810efe9f8dc49f967a656ac6
Ryan Oglesby <ryan.oglesby08@gmail.com>
2016-03-27 10:12:28 -0500

    Rename install as bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+~/.files/setup_terminal.sh
+
+~/.files/bootstrap/brew.sh
+~/.files/bootstrap/brew-cask.sh
--- setup_terminal.sh
+++ setup_terminal.sh
@@ -6,6 +6,3 @@ ln -sfv ~/.files/home/.inputrc ~
 ln -sfv ~/.files/tools/git/.gitconfig ~
 
 mkdir -p ~/.bundle && ln -sfv ~/.files/tools/bundler/config ~/.bundle
-
-~/.files/install/brew.sh
-~/.files/install/brew-cask.sh
