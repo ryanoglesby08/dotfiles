6ff277a8bb02c9b5bda4dab143396c2503931cf9
Ryan Oglesby <ryan.oglesby08@gmail.com>
2016-06-29 14:39:05 -0500

    symlink gitignore global

--- setup_terminal.sh
+++ setup_terminal.sh
@@ -3,6 +3,8 @@
 echo "Linking home directory dotfiles..."
 ln -sfv ~/.files/home/.bash_profile ~
 ln -sfv ~/.files/home/.inputrc ~
+
 ln -sfv ~/.files/tools/git/.gitconfig ~
+ln -sfv ~/.files/tools/git/.gitignore_global ~
 
 mkdir -p ~/.bundle && ln -sfv ~/.files/tools/bundler/config ~/.bundle
