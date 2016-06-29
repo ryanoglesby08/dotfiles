2f9daa89b7f2929286c005a7605c8cbee13965dc
Ryan Oglesby <ryan.oglesby08@gmail.com>
2016-06-29 14:27:03 -0500

    Adding initial npm settings

--- bootstrap/node.sh
+++ bootstrap/node.sh
@@ -4,4 +4,8 @@ echo "Updating npm..."
 
 npm install npm -g
 
+npm set init.author.email "ryan.oglesby08@gmail.com"
+npm set init.author.name "Ryan Oglesby"
+npm set init.license "MIT"
+
 echo "Done updating npm"
