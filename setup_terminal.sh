6e25261f88438bf570c9cada3250711843116a50
Ryan Oglesby <ryan.oglesby08@gmail.com>
2016-07-12 13:39:51 -0500

    Add ssh config

--- setup_terminal.sh
+++ setup_terminal.sh
@@ -5,6 +5,8 @@ ln -sfv ~/.files/home/.bash_profile ~
 ln -sfv ~/.files/home/.inputrc ~
 ln -sfv ~/.files/home/.vimrc ~
 
+mkdir -p ~/.ssh && ln -sfv ~/.files/tools/ssh/config ~/.ssh
+
 ln -sfv ~/.files/tools/git/.gitconfig ~
 ln -sfv ~/.files/tools/git/.gitignore_global ~
 
