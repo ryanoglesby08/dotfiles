7c66deac29ad1e694b6c36ad12fb3a369db079cf
Ryan Oglesby <ryan.oglesby08@gmail.com>
2018-02-21 20:46:22 -0800

    Generate ssh key

---
+++ bootstrap/ssh-keys.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Follows Github's steps: https://help.github.com/enterprise/2.12/user/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/
+
+generate-key() {
+  mkdir -p $HOME/.ssh
+  ssh-keygen -t rsa -b 4096 -C "ryan.oglesby08@gmail.com"
+
+  echo "Adding key to the ssh-agent to store the passphrase..."
+  eval "$(ssh-agent -s)"
+  ssh-add -K $HOME/.ssh/id_rsa
+
+  pbcopy < $HOME/.ssh/id_rsa.pub
+  echo "The contents of id_rsa.pub have been copied to the clipboard."
+  echo "--> Add the key to your Github account before continuing."
+  echo "--> Instructions can be found here: https://help.github.com/articles/adding-a-new-ssh-key-to-your-github-account/"
+  read -p "Press <enter> to continue... " -r
+
+  echo "done!"
+}
+
+# Check for existing id_rsa key
+if [[ $(ls $HOME/.ssh | grep ^id_rsa$) ]]; then
+  read -p "Found an id_rsa key in ~/.ssh. Do you want to generate one anyway? (y/n) " -r
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    exit 0
+  fi
+
+  generate-key
+else
+  read -p "No existing ssh key found. Do you want to generate one? (y/n) " -r
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    exit 0
+  fi
+
+  generate-key
+fi
--- go.sh
+++ go.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 mkdir -p $HOME/.files
-git clone git@github.com:ryanoglesby08/dotfiles.git $HOME/.files
+git clone https://github.com/ryanoglesby08/dotfiles.git $HOME/.files
 
 $HOME/.files/setup_terminal.sh
 
+$HOME/.files/bootstrap/ssh-keys.sh
 $HOME/.files/bootstrap/brew.sh
 $HOME/.files/bootstrap/brew-cask.sh
