aa38cdb8af228103097f8dff4cc4b9cf90d806b1
Ryan Oglesby <ryan.oglesby08@gmail.com>
2018-02-21 20:15:13 -0800

    Move all terminal home files into home folder

--- setup_terminal.sh
+++ setup_terminal.sh
@@ -6,7 +6,7 @@ ln -sfv $HOME/.files/home/.bash_profile $HOME
 ln -sfv $HOME/.files/home/.inputrc $HOME
 ln -sfv $HOME/.files/home/.vimrc $HOME
 
-mkdir -p $HOME/.ssh && ln -sfv $HOME/.files/tools/ssh/config $HOME/.ssh
+mkdir -p $HOME/.ssh && ln -sfv $HOME/.files/home/ssh/config $HOME/.ssh
 
-ln -sfv $HOME/.files/tools/git/.gitconfig $HOME
-ln -sfv $HOME/.files/tools/git/.gitignore_global $HOME
+ln -sfv $HOME/.files/home/git/.gitconfig $HOME
+ln -sfv $HOME/.files/home/git/.gitignore_global $HOME
