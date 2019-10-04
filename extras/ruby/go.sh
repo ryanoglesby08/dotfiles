b001334a696da6cb0ac735deb329298c50d9d149
Ryan Oglesby <ryan.oglesby08@gmail.com>
2019-10-04 10:14:43 -0500

    fix ruby bundler path. Closes #1

--- extras/ruby/go.sh
+++ extras/ruby/go.sh
@@ -5,7 +5,7 @@ echo "Installing and configuring ruby..."
 $HOME/.files/extras/ruby/install.sh
 
 echo "Configuring bundler"
-mkdir -p $HOME/.bundle && ln -sfv ./bundler/config $HOME/.bundle
+mkdir -p $HOME/.bundle && ln -sfv $HOME/.files/extras/ruby/bundler/config $HOME/.bundle
 
 echo "Linking rvm init script to bash profile"
 ln -sfv $HOME/.files/extras/ruby/.bashrc-ruby $HOME
