d9f8c3cb219eca32e6fd96746047b2531d86f709
Ryan Oglesby <ryan.oglesby08@gmail.com>
2019-11-09 18:42:56 -0600

    silence the login message for new terminal windows

--- bootstrap/terminal.sh
+++ bootstrap/terminal.sh
@@ -6,6 +6,9 @@ ln -sfv $HOME/.files/home/.zshrc $HOME
 ln -sfv $HOME/.files/home/.inputrc $HOME
 ln -sfv $HOME/.files/home/.vimrc $HOME
 
+# https://www.reddit.com/r/apple/comments/5t20tk/lpt_add_a_hushlogin_file_to_your_home_directory/
+ln -sfv $HOME/.files/home/.hushlogin $HOME
+
 mkdir -p $HOME/.ssh && ln -sfv $HOME/.files/home/ssh/config $HOME/.ssh
 
 ln -sfv $HOME/.files/home/git/.gitconfig $HOME
